fbd4e741f13f2ccceab5155e36e33e3dc3a02a44
Cryxwyxys <simonflohr1@gmx.de>
2026-09-26 16:26:05 +0200

    button Lib has a default button

--- Game/Buttons.py
+++ Game/Buttons.py
@@ -7,6 +7,7 @@ import time # for debugging, can probably removed in the end
 pygame.init()
 
 
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1000, 1000))
 
 pygame.display.set_caption("Raft1")
@@ -25,27 +26,49 @@ class Button():
         self.rect = pygame.Rect(xMiddle - width / 2, yMiddle - height / 2, width, height)
         self.surface = pygame.transform.scale(self.surface, (self.width, self.height))
 
+    def isHovering(self):
+        mousePos = pygame.mouse.get_pos()
+        self.hovering = self.rect.collidepoint(mousePos)
+
+    def displayHover(self):
+        self.greyOverlay = pygame.surface(self.width, self.height)
+        if isHovering():
+            self.greyoverlay
+
+
     def isClicked(self):
         mousePos = pygame.mouse.get_pos()
         
         self.hovering = self.rect.collidepoint(mousePos)
-        print(self.hovering)
         self.down = pygame.mouse.get_pressed()[0]
-        print(self.down)
         return self.hovering and self.down
 
     def render(self):
         screen.blit(self.surface, self.rect)
+        
+        pygame.draw.rect(screen, 'BLACK', self.rect, 5)
+        pygame.draw.rect(screen, 'RED', self.rect, 2)
 
 
 
-p = Button(50, 50 , 100, 100, "Assets/graphics/Buttons/redButton.png")
-p.render()
+testbutton = Button(50, 50 , 100, 100, "Assets/graphics/Buttons/redButton.png")
+testbutton.render()
 
 pygame.display.update()
 
-print(p.isClicked())
+if __name__ == "__main__":
+    while True: 
 
-while True: 
-    t = pygame.mouse.get_pos()
-    print(t)
+        testbutton.render()
+        pygame.display.update()
+        clock.tick(60)
+
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+        if testbutton.isClicked():
+
+            exit()
+
